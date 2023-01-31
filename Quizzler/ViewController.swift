2e311b85d72072e82baaf63e4e42c4348298496f
Alex Asichka <sanechka3@mail.ru>
2023-01-31 22:48:27 +0200

    add iboutlets and ibaction

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -9,35 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let stackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .vertical
-        stackView.distribution = .fillProportionally
-        stackView.spacing = 10
-        return stackView
-    }()
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var trueButton: UIButton!
+    
+    @IBOutlet weak var falseButton: UIButton!
+    
+    @IBOutlet weak var progressBar: UIProgressView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupUI()
-        
     }
     
-    func setupUI() {
-        view.backgroundColor = #colorLiteral(red: 0.1923559308, green: 0.2327082157, blue: 0.3624993563, alpha: 1)
-        view.addSubview(stackView)
-        setupConstraints()
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
     }
-
-    func setupConstraints() {
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            stackView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
-            stackView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor)
-        ])
-        
-    }
-
 }
